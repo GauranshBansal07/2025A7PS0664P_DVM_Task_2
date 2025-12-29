c21e94fd7d9ab62d07d7a21640256042531f347c
Macbook <admin@MacBook-Pro.local>
2025-12-29 12:12:31 +0530

    Fix UI with collectstatic and update hosts

--- config/settings.py
+++ config/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-change-me-later')
 
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost 127.0.0.1 [::1]').split(' ')
-
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
 # Application definition
 
 INSTALLED_APPS = [
