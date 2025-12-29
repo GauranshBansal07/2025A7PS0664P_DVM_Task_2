8a659d39d5facf7af9c08f0364e4aaa9be00eff7
Macbook <admin@MacBook-Pro.local>
2025-12-29 11:52:23 +0530

    Add whitenoise for static files

--- config/settings.py
+++ config/settings.py
@@ -132,6 +132,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
 
 AUTH_USER_MODEL = 'core.User'
 
